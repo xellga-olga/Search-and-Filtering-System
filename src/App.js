48a804e52b177c9682fa52ad196b7a0efbb43963
Noskina Olha <noskinaolga1@gmail.com>
2024-09-03 18:28:21 +0200

    Recommended section

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ function App() {
   return (
     <>
       <Navigation />
-      <Products />
       <Recommended />
+      <Products />
     </>
   );
 }
--- src/Recommended/Recommended.js
+++ src/Recommended/Recommended.js
@@ -2,7 +2,22 @@ import React from "react";
 import "./Recommended.css";
 
 function Recommended() {
-  return <div>Recommended</div>;
+  return (
+    <>
+    <div>
+      <h2 className="recommended-title">Recommended</h2>
+      <div className="recommended-flex">
+        <button className="btns">All Products</button>
+        <button className="btns">Nike</button>
+        <button className="btns">Adidas</button>
+        <button className="btns">Puma</button>
+        <button className="btns">Vans</button>
+        <button className="btns">Converse</button>
+        <button className="btns">New Balance</button>
+      </div>
+    </div>
+    </>
+  )
 }
 
 export default Recommended;

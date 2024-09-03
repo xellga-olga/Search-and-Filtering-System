d85289cbf64d261ee93acd9f3de788d47a067cd9
Noskina Olha <noskinaolga1@gmail.com>
2024-09-03 11:33:42 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,116 @@
+import { AiFillStar } from "react-icons/ai";
+
+const data = [
+   {
+      img: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcRIoG2bmgCUrG8achIHoHwMrU6on-P02F0ixHV4aXk-aED3Nj05',
+      title: 'Nike Air Monarch IV',
+      star: <AiFillStar className="rating-star" />,
+      reviews: '(120 reviews)',
+      prevPrice: '$130.00',
+      newPrice: '$100.00',
+      company: 'Nike',
+      color: 'white',
+      category: 'sneakers'
+   },
+   {
+      img: 'https://www.futobolt.hu/img/34373/FZ2762/500x500/FZ2762.webp?time=1717598529',
+      title: 'Adidas Ultraboost 21',
+      star: <AiFillStar className="rating-star" />,
+      reviews: '(250 reviews)',
+      prevPrice: '$180.00',
+      newPrice: '$150.00',
+      company: 'Adidas',
+      color: 'black',
+      category: 'running shoes'
+   },
+   {
+      img: 'https://s13emagst.akamaized.net/products/38288/38287897/images/res_92a9d7d684ef16a6fb6263e5ff6f2bf6.jpg?width=720&height=720&hash=ED91F69CD74E3B6DD303DFD197D72404',
+      title: 'Puma Suede Classic',
+      star: <AiFillStar className="rating-star" />,
+      reviews: '(95 reviews)',
+      prevPrice: '$85.00',
+      newPrice: '$65.00',
+      company: 'Puma',
+      color: 'red',
+      category: 'casual shoes'
+   },
+   {
+      img: 'https://encrypted-tbn2.gstatic.com/shopping?q=tbn:ANd9GcT3a811p0nVr2B2F_8z47ow-Ghzw-bgmxuGbDPkYsv3fA28AVlIfmfooMRIVZIjdMmgmW0JAqRZzLF4cZxkH7-YR0EjhRkVhtu5X9i5CZothaON&usqp=CAc',
+      title: 'Vans Old Skool',
+      star: <AiFillStar className="rating-star" />,
+      reviews: '(180 reviews)',
+      prevPrice: '$70.00',
+      newPrice: '$50.00',
+      company: 'Vans',
+      color: 'black/white',
+      category: 'skate shoes'
+   },
+   {
+      img: 'https://dr9l7gb9cebpv.cloudfront.net/media/catalog/product/cache/2774d42b8e55ba58cfe3fc9632392955/a/0/a04688c_a_107x1_2.jpg',
+      title: 'Converse Chuck Taylor All Star',
+      star: <AiFillStar className="rating-star" />,
+      reviews: '(300 reviews)',
+      prevPrice: '$60.00',
+      newPrice: '$45.00',
+      company: 'Converse',
+      color: 'white',
+      category: 'high-top sneakers'
+   },
+   {
+      img: 'https://nb.scene7.com/is/image/NB/ml574evg_nb_02_i?$dw_detail_main_lg$&bgc=f1f1f1&layer=1&bgcolor=f1f1f1&blendMode=mult&scale=10&wid=1600&hei=1600',
+      title: 'New Balance 574',
+      star: <AiFillStar className="rating-star" />,
+      reviews: '(220 reviews)',
+      prevPrice: '$90.00',
+      newPrice: '$75.00',
+      company: 'New Balance',
+      color: 'gray',
+      category: 'lifestyle shoes'
+   },
+   {
+      img: 'https://images.asics.com/is/image/asics/1011A767_402_SR_RT_GLB?$sfcc-product$',
+      title: 'Asics Gel-Kayano 27',
+      star: <AiFillStar className="rating-star" />,
+      reviews: '(145 reviews)',
+      prevPrice: '$160.00',
+      newPrice: '$130.00',
+      company: 'Asics',
+      color: 'blue',
+      category: 'running shoes'
+   },
+   {
+      img: 'https://images.reebok.eu/reebok-classic-leather-shoes_19729581_44793768_2048.jpg?c=1',
+      title: 'Reebok Classic Leather',
+      star: <AiFillStar className="rating-star" />,
+      reviews: '(175 reviews)',
+      prevPrice: '$75.00',
+      newPrice: '$55.00',
+      company: 'Reebok',
+      color: 'white',
+      category: 'casual shoes'
+   },
+   {
+      img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSb-pwWZgk4owJpN0hlnmq-uPyNrNzBqsPpRA&s',
+      title: 'Timberland 6-Inch Premium Waterproof Boot',
+      star: <AiFillStar className="rating-star" />,
+      reviews: '(200 reviews)',
+      prevPrice: '$190.00',
+      newPrice: '$170.00',
+      company: 'Timberland',
+      color: 'wheat',
+      category: 'boots'
+   },
+   {
+      img: 'https://static.ftshp.digital/img/p/4/2/4/5/1/9/424519-full_product.jpg',
+      title: 'Dr. Martens 1460',
+      star: <AiFillStar className="rating-star" />,
+      reviews: '(210 reviews)',
+      prevPrice: '$150.00',
+      newPrice: '$130.00',
+      company: 'Dr. Martens',
+      color: 'black',
+      category: 'boots'
+   }
+];
+
+export default data;

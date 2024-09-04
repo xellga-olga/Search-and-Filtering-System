8fa0ad555bb091aa7734880b75ea2d35bf6dc0eb
Noskina Olha <noskinaolga1@gmail.com>
2024-09-04 12:18:29 +0200

    Add category filter sidebar with radio options

--- src/Sidebar/Category/Category.js
+++ src/Sidebar/Category/Category.js
@@ -1,11 +1,42 @@
-import React from 'react'
-import './Category.css'
+import React from "react";
+import "./Category.css";
 
 function Category() {
   return (
-    <div>
-      Category
+    <div className="category">
+      <h2 className="sidebar-titile">Category</h2>
+      <div className="sidebar-items">
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>All
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Boots
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Casual shoes
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Running shoes
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Skate shoes
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>Lifestyle shoes
+        </label>
+        <label className="sidebar-label-container">
+          <input type="radio" name="test" />
+          <span className="checkmark"></span>High-top sneakers
+        </label>
+        <div className="line"></div>
+      </div>
     </div>
-  )
+  );
 }
-export default Category
+export default Category;
---
+++ src/Sidebar/Sidebar.js
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Sidebar.css";
+
+import Category from './Category/Category'
+import Price from './Price/Price'
+import Colors from './Colors/Colors'
+
+import { GiShoppingCart } from "react-icons/gi";
+
+function Sidebar() {
+  return (
+    <section className="sidebar">
+      <div className="logo-container">
+        <h1><GiShoppingCart/></h1>
+      </div>
+
+      <Category />
+      <Price />
+      <Colors />
+    </section>
+  );
+}
+
+export default Sidebar;
